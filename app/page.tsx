738d0589036156b9658f1d3709eb7f00ba9e1c9f
Niklas-Nordin <niklas.nordin@chasacademy.se>
2026-02-10 23:20:50 +0100

    Adding span to subtext

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ function Home() {
       </div>
       <div className="flex flex-col items-center justify-center w-full px-8 gap-16">
         <h2 className="text-5xl font-bold ">I'm <span className="bg-gradient-to-r from-orange-300 to-orange-500 bg-clip-text text-transparent">Niklas Nordin</span></h2>
-        <h3 className="w-full text-2xl text-center">Fullstack JavaScript developer focused on building clean, interactive web experiences with strong attention to design.</h3>
+        <h3 className="w-full text-2xl text-center">Fullstack JavaScript developer focused on building <span className="bg-gradient-to-r from-orange-300 to-orange-500 bg-clip-text text-transparent">clean, interactive</span> web experiences with strong attention to <span className="bg-gradient-to-r from-orange-300 to-orange-500 bg-clip-text text-transparent">design</span></h3>
       </div>
     </section>
   );
