9e1b3a36faf27662403a3db4e0a784f92150e51e
Niklas-Nordin <niklas.nordin@chasacademy.se>
2026-02-10 23:10:53 +0100

    Adding landingpage and navbar

---
+++ app/techstack/page.tsx
@@ -0,0 +1,9 @@
+function TechStack() {
+  return (
+    <div>
+      <h1>Tech Stack</h1>
+    </div>
+  );
+}
+
+export default TechStack;
---
+++ components/Navbar.tsx
@@ -0,0 +1,31 @@
+"use client"
+
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+
+function Navbar() {
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <nav className="relative w-full h-16 text-white bg-black flex items-center justify-between px-4">
+      <img className="w-40" src="./NordinDev-logo.png" alt="NordinDev logo" />
+
+      <div className={`flex gap-4 items-center justify-center ${isOpen ? "flex-col gap-8 bg-orange-400 text-xl w-[50%] items-center py-16 absolute right-0 top-0 z-20" : "hidden"} md:flex md:flex-row`}>
+        <Link href="/about">About</Link>
+        <Link href="/techstack">Tech Stack</Link>
+        <Link href="/projects">Projects</Link>
+        <Link href="/contact">Contact</Link>
+      </div>
+      {isOpen && 
+        <div className="absolute top-0 left-0 w-full h-screen z-15 bg-black/40 backdrop-blur-sm" onClick={() => setIsOpen(!isOpen)}></div> 
+      }
+      <button className="md:hidden cursor-pointer" onClick={() => setIsOpen(!isOpen)}>
+        { isOpen ? <img className="w-8 z-25 absolute top-4 right-4" src="./close.svg" alt="Closing navbar icon" /> : <img className={"w-8 z-25"} src="./hamburger-menu.svg" alt="Navigation menu button mobile" /> }
+      </button>
+    </nav>
+  );
+}
+
+export default Navbar;
