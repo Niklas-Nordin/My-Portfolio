54300fe9d484038ca1ccd6edf7442f1ac6f9e8e2
Niklas-Nordin <niklas.nordin@chasacademy.se>
2026-06-25 14:49:49 +0200

    Adding coor to selected page in navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,24 +1,31 @@
 "use client"
 
-import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
+import { usePathname } from "next/navigation";
 
 function Navbar() {
 
   const [isOpen, setIsOpen] = useState(false);
+  const pathname = usePathname();
 
+  const linkStyle = (href: string) => {
+      const isActive = pathname === href;
+      return isActive 
+        ? "text-brand-orange font-bold transition-colors duration-200" 
+        : "text-white hover:text-brand-orange hover:border-b hover:border-brand-orange transition-colors duration-200";
+    };
   return (
     <nav className="relative w-full h-16 text-white bg-black flex items-center justify-between px-4 border-b-5 border-brand-orange">
       <Link href="/">
         <img className="w-50" src="./NordinDev-logo.png" alt="NordinDev logo" />
       </Link>
 
-      <div className={`flex gap-8 items-center justify-center uppercase text-lg md:text-sm ${isOpen ? "flex-col gap-8 bg-brand-orange w-[50%] items-center py-16 absolute right-0 top-0 z-30" : "hidden"} md:flex md:flex-row`}>
-        <Link onClick={() => setIsOpen(false)} href="/">Home</Link>
-        <Link onClick={() => setIsOpen(false)} href="/about">About</Link>
-        <Link onClick={() => setIsOpen(false)} href="/projects">Projects</Link>
-        <Link onClick={() => setIsOpen(false)} href="/contact">Contact</Link>
+      <div className={`flex gap-8 items-center justify-center uppercase text-lg md:text-sm ${isOpen ? "flex-col gap-8 bg-black w-[50%] items-center py-16 absolute right-0 top-0 z-30" : "hidden"} md:flex md:flex-row`}>
+        <Link className={linkStyle("/")} onClick={() => setIsOpen(false)} href="/">Home</Link>
+        <Link className={linkStyle("/about")} onClick={() => setIsOpen(false)} href="/about">About</Link>
+        <Link className={linkStyle("/projects")} onClick={() => setIsOpen(false)} href="/projects">Projects</Link>
+        <Link className={linkStyle("/contact")} onClick={() => setIsOpen(false)} href="/contact">Contact</Link>
       </div>
       {isOpen && 
         <div className="absolute top-0 left-0 w-full h-screen z-20 bg-black/40 backdrop-blur-sm" onClick={() => setIsOpen(!isOpen)}></div> 
